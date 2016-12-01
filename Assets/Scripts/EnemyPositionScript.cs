cebc830fccb3f2965174428def6f925d8db7aa02
Pc-Komputer\Pc <pyszczakjakub@gmail.com>
2016-12-02 00:33:49 +0100

    MovingWall

---
+++ Assets/Scripts/EnemyPositionScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyPositionScript : MonoBehaviour {
+	public float x;
+	// Use this for initialization
+	void Start () {
+		 x = transform.position.x;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float t = Time.realtimeSinceStartup;
+		ChangePosition (t);
+	}
+	private void ChangePosition(float time)
+	{
+		gameObject.transform.position = new Vector3 (x + Mathf.Sin(time), transform.position.y, transform.position.z);
+	}
+}
--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -4,12 +4,7 @@ using System.Collections;
 public class GunScript : MonoBehaviour {
 	public GameObject bullet;
 	public float speedOfBullet;
-
-	void Start () {
-		Instantiate (bullet, transform.position, transform.rotation);
-	}
 	
-	// Update is called once per frame
 	void Update () 
 	{
 		Shoot ();
@@ -20,6 +15,7 @@ public class GunScript : MonoBehaviour {
 		{
 			GameObject clone = Instantiate (bullet, transform.position, transform.rotation) as GameObject;
 			clone.GetComponent<Rigidbody2D> ().velocity = new Vector2 (speedOfBullet, 0);
+			Destroy (clone, 2.0f);
 		}
 	}
 }
